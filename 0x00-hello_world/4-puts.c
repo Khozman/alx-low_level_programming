07a18a27142f6c6375aff5e9c407a477e7775f1d
khozman <bhekinkosikhosa@gmail.com>
2021-08-26 21:02:22 +0200

    Adding task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Prints string
+ * Description: This code Prints a string
+ * Return: 0
+ */
 int main(){
-  
-   puts("\"Programming is like building a multilingual puzzle");
-   return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
