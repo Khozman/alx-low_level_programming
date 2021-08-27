3bc05f3013febb91421c90a0f6d3685d892e58da
khozman <bhekinkosikhosa@gmail.com>
2021-08-27 15:42:13 +0200

    Adding task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *  * main - Main section
- *   * Description: This code prints alphabets except for e and q
- *    * Return: 0
- *     */
+ * main - Main section
+ * Description: This code prints alphabets except for e and q
+ * Return: 0
+ */
 int main(void)
 {
 	int i = 48;
 	int nextLine = 10;
-	
+
 	while (i <= 57)
 	{
 		putchar(i);
