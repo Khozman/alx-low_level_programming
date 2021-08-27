cb96fa779a6ad3f1ad5ffc19122d92734e27279b
khozman <bhekinkosikhosa@gmail.com>
2021-08-27 15:15:33 +0200

    Adding task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *  * main - Main section
+ *   * Description: This code prints alphabets
+ *    * Return: 0
+ *     */
+int main(void)
+{
+	int a = 97;
+	int emptyLine = 10;
+	int e = 101;
+	int q = 113;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+		if (a == 123)
+		{
+			putchar(emptyLine);
+		} 
+		else if( a == e)
+		{
+			a++;
+		} 
+		else if (a == q)
+		{
+			a++;
+		}
+	}			return (0);
+}
