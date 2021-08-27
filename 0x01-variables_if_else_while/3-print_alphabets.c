65053dbdd1755fa2224f69dc3d6f5c06209c969c
khozman <bhekinkosikhosa@gmail.com>
2021-08-27 14:56:07 +0200

    Adding task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,6 @@
  * Description: This code prints alphabets
  * Return: 0
  */
-void printCharacter(int i);
 int main(void)
 {
 	int lowerCase = 97;
@@ -14,27 +13,18 @@ int main(void)
 
 	while (lowerCase < 123)
 	{
-		printCharacter(lowerCase);
+		putchar(lowerCase);
 		lowerCase++;
 
 		while (lowerCase == 123 && upperCase < 91)
 		{
-			printCharacter(upperCase);
+			putchar(upperCase);
 			upperCase++;
 			if (upperCase == 91)
 			{
-				printCharacter(newLine);
+				putChar(newLine);
 			}
 		}
 	}
 	return (0);
-}
-/**
- * printCharacter - function
- * Description: This code is resposible for printing character
- * Return: void
- */
-void printCharacter(int i)
-{
-	putchar(i);
 }
